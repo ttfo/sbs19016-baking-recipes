b6cd6c07e5cbbf73ffaac26e9bd1d2eab79d9f77
ttfo <puntogif@gmail.com>
2020-08-06 02:36:40 +0100

    end-point added

--- src/main/java/ie/cct/sbs19016bakingrecipes/BakingAppController.java
+++ src/main/java/ie/cct/sbs19016bakingrecipes/BakingAppController.java
@@ -3,23 +3,46 @@ package ie.cct.sbs19016bakingrecipes;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import ie.cct.model.Author;
 import ie.cct.model.Recipe;
+import ie.cct.sbs19016bakingrecipes.LocalJsonParser;
 
 
 //https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/web/bind/annotation/RestController.html
 @RestController
 public class BakingAppController {
 	
-	private List<Recipe> myBakingRecipes = new ArrayList<Recipe>();
+	
+	// Populate with initial recipes
+	LocalJsonParser myJP = new LocalJsonParser();
+	List<Recipe> myBakingRecipes = myJP.myRecipeBuilder();
+	
 	
 	public BakingAppController() {
 		
-		// Populate with initial recipes
-		LocalJsonParser myJP = new LocalJsonParser();
-		//myBakingRecipes = myJP.myRecipeBuilder();
+		for (Recipe r : myBakingRecipes) {
+			Author author = new Author();
+			author.setName("Miriam");
+			author.setWebsite("udacity.com"); // provider of recipes in JSON format
+			r.setAuthor(author);
+			
+			//System.out.println(r.getName()); //testing
+		}		
 		
+	}
+	
+	
+	
+	/*
+	 * END-POINT => "Get my recipes"
+	 * Returns list of recipes in JSON format
+	 */	
+	@GetMapping("get-recipes") // http://localhost:8080/get-livestock
+	public List<Recipe> getRecipes() { 
+		return myBakingRecipes;
 	}
 	
 
--- src/main/java/ie/cct/sbs19016bakingrecipes/Sbs19016BakingRecipesApplication.java
+++ src/main/java/ie/cct/sbs19016bakingrecipes/Sbs19016BakingRecipesApplication.java
@@ -13,21 +13,6 @@ public class Sbs19016BakingRecipesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Sbs19016BakingRecipesApplication.class, args);
-		
-		// Populate with initial recipes
-		LocalJsonParser myJP = new LocalJsonParser();
-		List<Recipe> myBakingRecipes = myJP.myRecipeBuilder();
-		System.out.println(myBakingRecipes.toString());
-		
-		for (Recipe r : myBakingRecipes) {
-			Author author = new Author();
-			author.setName("Miriam");
-			author.setWebsite("udacity.com"); // provider of recipes in JSON format
-			r.setAuthor(author);
-			
-			// System.out.println(r.getName()); //testing
-			
-		}
 	}
 
 }
